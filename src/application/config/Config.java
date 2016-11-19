bec03b4e61b14d3c33a6e2e31b00875b0f3325a6
jiro <gitromaru666>
2016-11-19 20:11:45 +0900

    デバッグ用にtoStringをOverride

--- src/application/config/Config.java
+++ src/application/config/Config.java
@@ -9,4 +9,8 @@ public class Config {
   public String projectPath;
   public String inputPath;
 
+  @Override
+  public String toString() {
+    return String.format("projectPath: %s, inputPath: %s", projectPath, inputPath);
+  }
 }
