8b08f8c314263c0b8d73cef76102c26bf1ac726b
jiro <gitromaru666>
2016-11-19 17:07:38 +0900

    RPGツクールのプロジェクトフォルダの設定を変更するレイアウトを部分的に実装。

---
+++ src/application/config/import_file/ImportFileVBoxController.java
@@ -0,0 +1,12 @@
+package application.config.import_file;
+
+import javafx.fxml.FXML;
+
+public class ImportFileVBoxController {
+  public static final String DESCRIPTION = "インポートするファイルの場所を変更します。";
+
+  @FXML
+  private void initialize() {
+
+  }
+}
