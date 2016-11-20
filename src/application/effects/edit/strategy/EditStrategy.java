20d115fa371f81c053309b5c5e1033c6a583d66a
jiro <gitromaru666>
2016-11-21 00:33:31 +0900

    各種戦略クラスを作成。ただし未実装。

--- src/application/effects/edit/strategy/EditStrategy.java
+++ src/application/effects/edit/strategy/EditStrategy.java
@@ -3,8 +3,30 @@ package application.effects.edit.strategy;
 /**
  * 使用効果のテキストを編集する戦略インタフェース。
  * @author jiro
- *
  */
-public interface EditStrategy {
-  public String formatContent(int codeId, int dataId, double value1, double value2);
+public abstract class EditStrategy {
+  public abstract String formatToContentText(int codeId, int dataId, double value1, double value2);
+  public abstract String convertJsonString();
+
+  /**
+   * 戦略クラスのインスタンス切り替えのためのインデックスを取得する。
+   * @param codeId CodeId
+   * @return 戦略インデックス
+   */
+  public static int getStrategyIndexFrom(int codeId) {
+    int placeTen = codeId / 10 - 1;
+    int placeOne = codeId % 10 - 1;
+    // @formatter:off
+    placeOne =
+          placeTen == 0 ? placeOne
+        : placeTen == 1 ? placeOne + 3
+        : placeTen == 2 ? placeOne + 5
+        : placeOne + 9;
+    // @formatter:on
+    return placeOne;
+  }
+
+  public static void changeStrategy() {
+
+  }
 }
---
+++ src/application/effects/edit/strategy/MPHealStrategy.java
@@ -0,0 +1,15 @@
+package application.effects.edit.strategy;
+
+public class MPHealStrategy extends EditStrategy {
+
+  @Override
+  public String formatToContentText(int codeId, int dataId, double value1, double value2) {
+    return null;
+  }
+
+  @Override
+  public String convertJsonString() {
+    return null;
+  }
+
+}
