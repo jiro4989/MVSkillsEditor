85c129788144d069bef7c2421ca437e74bab49a1
jiro <gitromaru666>
2016-11-20 14:03:18 +0900

    コントローラクラスにリストを登録するように変更。

--- src/application/effects/edit/EditStage.java
+++ src/application/effects/edit/EditStage.java
@@ -1,5 +1,7 @@
 package application.effects.edit;
 
+import java.util.List;
+
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
@@ -8,12 +10,12 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 public class EditStage extends Stage {
-  public EditStage(int codeId, int dataId, double value1, double value2) {
+  public EditStage(int codeId, int dataId, double value1, double value2, List<String> skillList, List<String> stateList, List<String> commonEventList) {
     try {
       FXMLLoader loader = new FXMLLoader(getClass().getResource("EditStage.fxml"));
       BorderPane root = (BorderPane) loader.load();
       EditStageController controller = (EditStageController) loader.getController();
-      controller.setInitialValues(codeId, dataId, value1, value2);
+      controller.setInitialValues(codeId, dataId, value1, value2, skillList, stateList, commonEventList);
       Scene scene = new Scene(root, 500, 500);
       scene.getStylesheets()
           .add(getClass().getResource("/application/application.css").toExternalForm());
